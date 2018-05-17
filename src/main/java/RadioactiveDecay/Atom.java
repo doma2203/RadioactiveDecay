9cb3c00ded9d31823c90a44cd569209b4700ac0b
Weronika Naziemiec <wnaziemiec@gmail.com>
2018-05-17 19:14:23 +0200

    zmieniony Atom, dodane Sample, Experiment

--- src/main/java/RadioactiveDecay/Atom.java
+++ src/main/java/RadioactiveDecay/Atom.java
@@ -1,53 +1,56 @@
 package RadioactiveDecay;
 
-import java.util.*;
-
-
-class Parameter extends HashMap<String, Float> {
-    private Boolean isCorrectKey(String key, String pattern) {
-        return (key.compareToIgnoreCase(pattern) == 0);
-    }
-
-    @Override
-    public Float put(String key, Float value) {
-        if (this.size() < 1 || isCorrectKey(key, "halflife") || isCorrectKey(key, "meanlifetime") || isCorrectKey(key, "decayconstant")) {
-            return super.put(key, value);
-        } else
-            throw new IllegalArgumentException("Niepoprawny argument!");
-    }
-}
-
 public class Atom implements IAtom {
 
     int AtomicNumber;
     String DisplayedName;
-    Parameter Parameter = new Parameter();
+    Parameter parameter = new Parameter();
     Boolean Undergone = false;
 
     public Atom(int no, String name, String parameter_name, Float parameter_value) {
         this.AtomicNumber = no;
         this.DisplayedName = name;
-        this.Parameter.put(parameter_name, parameter_value);
+        this.parameter.put(parameter_name, parameter_value);
     }
 
-    public int getAtomicNumber() {
+//    public Atom(Atom atom) {
+//        try
+//        {
+//            this = atom.clone();
+//        }
+//        catch(CloneNotSupportedException e )
+//        {
+//
+//        }
+//    }
 
+    public int getAtomicNumber()
+    {
         return this.AtomicNumber;
     }
 
-    public String getDisplayedName() {
+    public String getDisplayedName()
+    {
         return DisplayedName;
     }
 
-    public String getParameter() {
-        return Parameter.keySet().toString(); //TODO: Delete brackets in string!
+    public String getParameter()
+    {
+        return parameter.keySet().toString(); //TODO: Delete brackets in string!
     }
 
-    public void Undergone() {
+    public float get(String parameterName)
+    {
+        return parameter.get(parameterName);
+    }
+
+    public void Undergone()
+    {
         Undergone = true;
     }
 
-    public Boolean getState() {
+    public Boolean getState()
+    {
         return Undergone;
     }
 }
---
+++ src/main/java/RadioactiveDecay/Experiment.java
@@ -0,0 +1,75 @@
+package RadioactiveDecay;
+import java.util.*;
+
+public class Experiment extends Thread
+{
+    private Sample sample;
+    private Date t0;
+    private long duration;
+    private int N0;
+    private int N;
+    private float P;
+    private float A;
+
+    Experiment(long duration, int size, int no, String name, String parameter_name, Float parameter_value)
+    {
+        this.sample = new Sample(size, no, name, parameter_name, parameter_value);
+        this.t0= new Date();
+        this.duration = duration*1000; //bylo w sekundach, zmieniam na milisekundy
+        this.N0 = size;
+    }
+
+//    Experiment(long duration, Sample sample)
+//    {
+//
+//    }
+//    Experiment(long duration, int size, Atom atom)
+//    {
+//
+//    }
+
+    int remainingParticles()
+    {
+        float halfLife = sample.get("halfLife");
+        float t = (new Date()).getTime() - t0.getTime();
+        N = (int) (N0 * Math.pow( 0.5, (t/halfLife/1000) ));
+        //System.out.println("pow: " + Math.pow( 0.5, (t/halfLife/1000)));
+        return N;
+    }
+
+    float surviveProbability()
+    {
+        P = (float)N/N0;
+        return P;
+    }
+
+    float radiologicalActivity()
+    {
+        A = sample.get("decayConstant") * N;
+        return A;
+    }
+
+    @Override
+    public void run()
+    {
+        float t = (new Date()).getTime() - t0.getTime();
+        while ( t <= duration)
+        {
+            try {
+                sleep(1000);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(t/1000);
+            System.out.println(remainingParticles());
+            System.out.println(surviveProbability());
+            System.out.println(radiologicalActivity());
+            System.out.println();
+            t = (new Date()).getTime() - t0.getTime();
+
+        }
+    }
+
+}
